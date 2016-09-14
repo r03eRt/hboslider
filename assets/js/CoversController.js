81e98bfbeb9377e6720bb8b898acc12e74486a5e
r03eRt <morgadoluengo@gmail.com>
2016-09-14 17:28:40 +0200

    added search slider

--- assets/js/CoversController.js
+++ assets/js/CoversController.js
@@ -4,7 +4,9 @@ app.controller('CoversController',['coverService',function(coverService) {
     this.selected = null;
     this.covers = [];
 
-    this.current = this.covers[2];
+    this.current = 2;
+    console.log('puntero --> '+this.current);
+
 
     this.covers = [
         {
@@ -12,88 +14,98 @@ app.controller('CoversController',['coverService',function(coverService) {
             avatar: '',
             content: '',
             active: false,
-            offset: -2
+            offset: -2,
+            value:''
         },
         {
             name: '',
             avatar: '',
             content: '',
             active: false,
-            offset: -1
+            offset: -1,
+            value:''
         },
 
         {
-            name: 'Fear of the walking dead',
+            name: 'AFear of the walking dead',
             avatar: 'walkingdead',
             content: 'walkingdead',
             active: true,
-            offset: 0
+            offset: 0,
+            value:1
         },
         {
             name: 'Billions',
             avatar: 'billions',
             content: 'billions',
             active: false,
-            offset: 1
+            offset: 1,
+            value:2
         },
         {
-            name: 'girls',
+            name: 'Cgirls',
             avatar: 'girls',
             content: 'girls',
             active: false,
-            offset: 2
+            offset: 2,
+            value:3
         },
         {
-            name: 'Los soprano',
+            name: 'DLos soprano',
             avatar: 'los_soprano',
             content: 'los soprano',
             active: false,
-            offset: 3
+            offset: 3,
+            value:4
         },
         {
-            name: 'true detective',
+            name: 'Etrue detective',
             avatar: 'true_detective',
             content: 'walkingdead',
             active: false,
-            offset: 4
+            offset: 4,
+            value:5
         },
         {
-            name: 'blacksails',
+            name: 'Fblacksails',
             avatar: 'blacksails',
             content: 'blacksails',
             active: false,
-            offset: 5
+            offset: 5,
+            value:6
         },
         {
-            name: 'Fear of the walking dead2',
+            name: 'GFear of the walking dead2',
             avatar: 'walkingdead2',
             content: 'walkingdead2',
             active: false,
-            offset: 6
+            offset: 6,
+            value:7
         },
         {
-            name: 'Billions2',
+            name: 'HBillions2',
             avatar: 'billions2',
             content: 'billions2',
             active: false,
-            offset: 7
+            offset: 7,
+            value:8
         },
         {
-            name: 'girls2',
+            name: 'Igirls2',
             avatar: 'girls2',
             content: 'girls2',
             active: false,
             offset: 8
         },
         {
-            name: 'Los soprano2',
+            name: 'JLos soprano2',
             avatar: 'los_soprano2',
             content: 'los soprano2',
             active: false,
             offset: 9
         },
         {
-            name: 'true detective2',
+            name: 'Ktrue detective2',
             avatar: 'true_detective2',
             content: 'walkingdead2',
             active: false,
@@ -136,90 +148,134 @@ app.controller('CoversController',['coverService',function(coverService) {
         });**/
 
 
+    this.changeCurrent = function (new_index) {
+        console.log(this.current, this.covers);
+        var current_index = 0;
+        console.log(new_index);
+        for (var i = 0, l = this.covers.length; i < l; i++) {
+            this.covers[i].offset = this.covers[i].offset-new_index ;
+            TweenLite.to('#film_'+i, 1.2, {x: '-='+new_index*100+'%', ease:Power4.easeInOut});
+            TweenLite.to('.card', 0.5, {className:'-=active', scale:0.85});
+
+            if(this.covers[i].offset === 0){
+              current_index = i;
+            }
+        }
+        TweenLite.to('#film_'+current_index, 1.8, {className:'+=active', scale:0.95});
+        this.current = current_index;
+    };
+    
+    this.move = function ($event,$index_click) {
+        //console.log('puntero --> '+this.current);
+
+        //console.log('click '+$index_click);
+        //console.log(this.covers[$index_click]);
+
+        var movement = this.covers[this.current].offset + this.covers[$index_click].offset;
+
+
+        if(movement === 1){
+            this.moverDerecha($event,1);
+        }else if(movement === 2){
+            this.moverDerecha($event,2);
+        }else if(movement === -1){
+            this.moverIzquierda($event,1);
+        }else if(movement === -2){
+            this.moverIzquierda($event,2);
+        }else if(movement === 0){
+            console.log("No hago nada");
+        }
+
+
+
+
+
+    };
 
     
 
-    this.moverDerecha = function ($event) {
-        //console.log(this.covers[0].offset);
-        // actualizamos desplazamiento ++
-        //console.log(this.covers[this.covers.length-1].offset);
-        if(this.covers[this.covers.length-1].offset !== 2){
-            for (var i = 0, l = this.covers.length; i < l; i++) {
-                this.covers[i].offset-- ;
-                TweenLite.to('#film_'+i, 1.2, {x: '-=100%', ease:Power4.easeInOut});
-                if(this.covers[i].offset === 0){
-                    TweenLite.to('#film_'+i, 0.5, {className:'+=active', scale:0.95});
+    this.moverDerecha = function ($event, times) {
 
-                    TweenLite.to('#film_'+(i+1), 0.5, {className:'-=active', scale:0.85});
-                    TweenLite.to('#film_'+(i-1), 0.5, {className:'-=active', scale:0.85});
+        if(this.covers[this.covers.length-1].offset !== 2){
+            if(times === 2){
+                for (var i = 0, l = this.covers.length; i < l; i++) {
+                    this.covers[i].offset-- ;
+                    TweenLite.to('#film_'+i, 1.2, {x: '-=100%', ease:Power4.easeInOut});
+                    if(this.covers[i].offset === 0){
+                        TweenLite.to('#film_'+i, 0.5, {className:'+=active', scale:0.95});
+                        TweenLite.to('#film_'+(i+1), 0.5, {className:'-=active', scale:0.85});
+                        TweenLite.to('#film_'+(i-1), 0.5, {className:'-=active', scale:0.85});
+                        this.current = i;
+                    }
+                }
+                for (var i = 0, l = this.covers.length; i < l; i++) {
+                    this.covers[i].offset-- ;
+                    TweenLite.to('#film_'+i, 1.2, {x: '-=200%', ease:Power4.easeInOut});
+                    if(this.covers[i].offset === 0){
+                        TweenLite.to('#film_'+i, 0.5, {className:'+=active', scale:0.95});
+                        TweenLite.to('#film_'+(i+1), 0.5, {className:'-=active', scale:0.85});
+                        TweenLite.to('#film_'+(i-1), 0.5, {className:'-=active', scale:0.85});
+                        this.current = i;
+                    }
+                }
+            }else{
+                for (var i = 0, l = this.covers.length; i < l; i++) {
+                    this.covers[i].offset-- ;
+                    TweenLite.to('#film_'+i, 1.2, {x: '-=100%', ease:Power4.easeInOut});
+                    if(this.covers[i].offset === 0){
+                        TweenLite.to('#film_'+i, 0.5, {className:'+=active', scale:0.95});
+                        TweenLite.to('#film_'+(i+1), 0.5, {className:'-=active', scale:0.85});
+                        TweenLite.to('#film_'+(i-1), 0.5, {className:'-=active', scale:0.85});
+                        this.current = i;
+                    }
                 }
             }
+
         }
 
-        //console.log(this.covers);
+        console.log('puntero --> '+this.current);
 
-
-
-
-        //$('.active').removeClass('active');
-        //$($event.currentTarget).addClass('active');
-        //var scrollTo = $($event.currentTarget)        // retrieve the jquery ref to the div
-        //    .toggleClass('active')       // change its bg
-        //    .position().left;
-
-        //console.log($($event.currentTarget).offset().left - ( $(window).width() - $($event.currentTarget).outerWidth(true) ) / 2 );
-
-        //$('.scroller').animate({ scrollLeft: $($event.currentTarget).offset().left - ( $(window).width() - $($event.currentTarget).outerWidth(true) ) / 2  }, 800);
-
-
-        //$('html,body').animate({ scrollTop: $(this).offset().top - ( $(window).height() - $(this).outerHeight(true) ) / 2  }, 200);
-
-
-        //$($event.currentTarget).toggleClass('active');
-        //$($event.currentTarget.nextElementSibling).removeClass('active');
-        //$($event.currentTarget.previousElementSibling).removeClass('active');
     }
 
-    this.moverIzquierda = function () {
-        //console.log(this.covers[0].offset);
+    this.moverIzquierda = function ($event,times) {
 
-
-
-
-        //console.log(this.covers[0].offset);
         if(this.covers[0].offset !== -2){
-            for (var i = 0, l = this.covers.length; i < l; i++) {
-                this.covers[i].offset++ ;
-                TweenLite.to('#film_'+i, 1.2, {x: '+=100%', ease:Power4.easeInOut});
-                if(this.covers[i].offset === 0){
-                    TweenLite.to('#film_'+i, 0.5, {className:'+=active', scale:0.95});
-                    TweenLite.to('#film_'+(i+1), 0.5, {className:'-=active', scale:0.85});
-                    TweenLite.to('#film_'+(i-1), 0.5, {className:'-=active', scale:0.85});
+            if(times === 2){
+                for (var i = 0, l = this.covers.length; i < l; i++) {
+                    this.covers[i].offset++ ;
+                    TweenLite.to('#film_'+i, 1.2, {x: '+=100%', ease:Power4.easeInOut});
+                    if(this.covers[i].offset === 0){
+                        TweenLite.to('#film_'+i, 0.5, {className:'+=active', scale:0.95});
+                        TweenLite.to('#film_'+(i+1), 0.5, {className:'-=active', scale:0.85});
+                        TweenLite.to('#film_'+(i-1), 0.5, {className:'-=active', scale:0.85});
+                        this.current = i;
+                    }
+                }
+                for (var i = 0, l = this.covers.length; i < l; i++) {
+                    this.covers[i].offset++ ;
+                    TweenLite.to('#film_'+i, 1.2, {x: '+=200%', ease:Power4.easeInOut});
+                    if(this.covers[i].offset === 0){
+                        TweenLite.to('#film_'+i, 0.5, {className:'+=active', scale:0.95});
+                        TweenLite.to('#film_'+(i+1), 0.5, {className:'-=active', scale:0.85});
+                        TweenLite.to('#film_'+(i-1), 0.5, {className:'-=active', scale:0.85});
+                        this.current = i;
+                    }
+                }
+            }else{
+                for (var i = 0, l = this.covers.length; i < l; i++) {
+                    this.covers[i].offset++ ;
+                    TweenLite.to('#film_'+i, 1.2, {x: '+=100%', ease:Power4.easeInOut});
+                    if(this.covers[i].offset === 0){
+                        TweenLite.to('#film_'+i, 0.5, {className:'+=active', scale:0.95});
+                        TweenLite.to('#film_'+(i+1), 0.5, {className:'-=active', scale:0.85});
+                        TweenLite.to('#film_'+(i-1), 0.5, {className:'-=active', scale:0.85});
+                        this.current = i;
+                    }
                 }
             }
+
         }
-
-        //console.log(this.covers);
-
-
-
-        //$('.active').removeClass('active');
-        //$($event.currentTarget).addClass('active');
-        //var scrollTo = $($event.currentTarget)        // retrieve the jquery ref to the div
-        //    .toggleClass('active')       // change its bg
-        //    .position().left;
-
-        //console.log($($event.currentTarget).offset().left - ( $(window).width() - $($event.currentTarget).outerWidth(true) ) / 2 );
-
-        //$('.scroller').animate({ scrollLeft: $($event.currentTarget).offset().left - ( $(window).width() - $($event.currentTarget).outerWidth(true) ) / 2  }, 800);
-
-
-        //$('html,body').animate({ scrollTop: $(this).offset().top - ( $(window).height() - $(this).outerHeight(true) ) / 2  }, 200);
-
-
-        //$($event.currentTarget).toggleClass('active');
-        //$($event.currentTarget.nextElementSibling).removeClass('active');
-        //$($event.currentTarget.previousElementSibling).removeClass('active');
+        console.log('puntero --> '+this.current);
     }
 
 
